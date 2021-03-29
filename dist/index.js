5b7d5a706854d16f6d5cd263c881b74f0fdfbb55
Halit Ogunc <halit.ogunc@numberfour.eu>
2021-03-29 11:48:03 +0200

    use peerDependencies

--- dist/index.js
+++ dist/index.js
@@ -13,6 +13,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.QRCode = void 0;
 var isEqual = require("lodash.isequal");
 var qrGenerator = require("qrcode-generator");
 var React = require("react");
